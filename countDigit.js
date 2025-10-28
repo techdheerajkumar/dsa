0989dca780eb2204a04f70d4b82877d4d42c79ca
Dheeraj Kumar <ui.dheerajkumar@gmail.com>
2025-10-28 09:42:32 +0530

    DSA Module 1 - Count digits

--- countDigit.js
+++ countDigit.js
@@ -1,5 +1,12 @@
-let num = 259;
-let total = 0;
-for(let i = 0; i<=num.length; i++){
-   console.log(num[i])
+function countDigit(num) {
+    let count = 0;
+    num = Math.abs(num) // Handling negative numbers (converting negative numbers to positive)
+    if (num === 0) return 1; // Handling 0 number case
+    let n = Math.floor(num); // Handling decimal numbers
+    while (n > 0) {
+        n = Math.floor(n / 10)
+        count++
+    }
+    return count;
 }
+console.log(countDigit(3331))
